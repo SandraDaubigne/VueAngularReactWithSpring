e875a57633edc67a49115301556f52ce69d4a0c7
Sandra Daubigne <44670927+SandraDaubigne@users.noreply.github.com>
2020-11-13 13:27:01 +0100

    update

--- SpringWithHibernate/src/main/java/com/example/springwithhibernate/TicketRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.springwithhibernate;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TicketRepository  extends CrudRepository<Ticket, Integer> {
-}
---
+++ VUE/src/test/java/com/example/advancedjava/AdvancedjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advancedjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedjavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
